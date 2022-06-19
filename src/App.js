97121cfc2ac13aee216675d7434ac9917155bb7a
chethancm2001 <chethancm2001kadur@gmail.com>
2022-06-19 13:22:59 +0530

    online c complier

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Editor from "@monaco-editor/react";
+import { useState } from "react";
+import Axios from "axios";
+import "./sty.css";
 function App() {
+  let [Code, setCode] = useState(" ");
+  let [Output, setOutput] = useState(" ");
+  async function submitbtn() {
+    let result = await Axios.post("http://localhost:4000/api", { code: Code });
+    console.log(result.data.output);
+    setOutput(result.data.output);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>online c complier</h1>
+      <Editor
+        height="70vh"
+        width="100%"
+        theme="vs-dark"
+        language="c"
+        defaultLanguage="c"
+        defaultValue="/*write code here*/"
+        onChange={(value) => {
+          setCode(value);
+        }}
+      />
+      <button onClick={submitbtn}>Run</button>
+      <div className="out-put">
+        Output
+        <h4> {Output}</h4>{" "}
+      </div>
     </div>
   );
 }
